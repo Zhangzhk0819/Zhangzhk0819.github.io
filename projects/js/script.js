447ff81f19aaebb80f1220dc73c6475ad7fe0301
zhikun.zhang <1510856038@qq.com>
2023-04-07 15:12:35 -0700

    Site updated: 2023-04-07 15:12:34

--- projects/js/script.js
+++ projects/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
     $('.res-link').attr('target', '_blank');
     // $('.res-link').eq(2).attr('target', '_blank');
+    $('.authors').attr('target', '_blank');
+    $('h1').attr('target', '_blank');
+    $('h2').attr('target', '_blank');
 });
